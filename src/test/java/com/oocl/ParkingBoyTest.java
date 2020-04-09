3a88d73cb1db43d599112e83678727fed81c584c
lokmingjay <jay.ngan@oocl.com>
2020-04-09 15:54:07 +0800

    [NGANJA] parkingboy should return car when given ticket

--- src/test/java/com/oocl/ParkingBoyTest.java
+++ src/test/java/com/oocl/ParkingBoyTest.java
@@ -12,4 +12,16 @@ public class ParkingBoyTest {
         ParkingTicket ticket = parkingBoy.park(car);
         Assert.assertNotNull(ticket);
     }
+
+    @Test
+    public void should_return_car_when_given_ticket(){
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+        ParkingTicket ticket = parkingBoy.park(car);
+        Car fetchCar = parkingBoy.fetch(ticket);
+        Assert.assertEquals(car,fetchCar);
+    }
+
+
 }
